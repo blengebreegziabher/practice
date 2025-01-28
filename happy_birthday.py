30bf17e33ae83b9992e4bc6b47e7d33154092a55
blen g <bg6526@rit.edu>
2025-01-28 20:14:56 +0400

    tuesday

---
+++ happy_birthday.py
@@ -0,0 +1,25 @@
+'''
+name= input("what is your name:")
+birthmonth= input(" what month were you born:")
+birthday= input("what is your birth day of the month:")
+birthyear= input("what is your birth year:")
+print(name  , birthmonth , birthday , birthyear)
+'''
+'''
+name= input("what is your name:")
+birthmonth= input(" what month were you born:")
+birthday= input("what is your birth day of the month:")
+birthyear= input("what is your birth year:")
+print(name , ", you birthday is on " , birthmonth , birthday , birthyear)
+'''
+
+def b_day():
+    name= input("what is your name:")
+    birthmonth= input(" what month were you born:")
+    birthday= input("what is your birth day of the month:")
+    birthyear= input("what is your birth year:")
+    print(name , ", you birthday is on " , birthmonth , birthday , birthyear)
+b_day()
+b_day()
+b_day()
+
